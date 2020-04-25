654589de11dc9787e8488a96792235ba7c3d3f44
borpin <brian.orpin@gmail.com>
2020-04-25 11:14:55 +0100

    Revert "Change time property on Influx output to timestamp"
    This reverts commit 7dd244b88f98a32c037dcb6b2833f7719d19b0ee.

--- octopus.js
+++ octopus.js
@@ -108,7 +108,7 @@ module.exports = function(RED) {
 
                                 msg.payload.results.forEach(function(item, index) {
                                     msg3.payload.push([{ value_inc_vat : item.value_inc_vat, 
-                                                        "timestamp": new Date(item.valid_from).getTime() *1000 *1000}, {"source" : "Agile"}]);
+                                                        "time": new Date(item.valid_from).getTime() *1000 *1000}, {"source" : "Agile"}]);
                                 });
 
                                 msg3.measurement = "OctopusPrice";
