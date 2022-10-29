456cb410cbab9e402b122a40c39b11c1529eabc4
TJ Saunders <tjsaunders@users.noreply.github.com>
2022-10-29 19:33:12 +0100

    Add tag for meter type. Change measurement name

--- octopus.js
+++ octopus.js
@@ -132,10 +132,10 @@ module.exports = function(RED) {
                                 var msg3 = {};
                                 msg3.payload = [];
 								msg.payload.results.forEach(function(item, index) {
-									msg3.payload.push([{ value_inc_vat : item.value_inc_vat, "time": new Date(item.valid_from).getTime() *1000 *1000},{"identifier": n.comboidentifier, data: "tariff", source: influxDBsource}]);
+									msg3.payload.push([{ value_inc_vat : item.value_inc_vat, "time": new Date(item.valid_from).getTime() *1000 *1000},{"identifier": n.comboidentifier, data: "tariff", meter: "electricity" , source: influxDBsource}]);
                                 });
 								
-                                msg3.measurement = "OctopusPrice";
+                                msg3.measurement = "octopus";
 
                                 next_run = next_half_hour;
 								
@@ -169,9 +169,9 @@ module.exports = function(RED) {
 													msg4.payload = [];
 												
 													outputx.payload.results.forEach(function(item, index) {												
-														msg4.payload.push([{ consumption : item.consumption,  "time": new Date(item.interval_start).getTime() *1000 *1000},{"identifier": n.comboidentifier, data: "consumption", source: influxDBsource}]);
+														msg4.payload.push([{ consumption : item.consumption,  "time": new Date(item.interval_start).getTime() *1000 *1000},{"identifier": n.comboidentifier, data: "consumption", meter: "electricity", source: influxDBsource}]);
 													});											
-													msg4.measurement = "OctopusConsumption";
+													msg4.measurement = "octopus";
 													 node.send([msg, msg2, msg3, msg4]);
 												} catch(err) {
 													node.error(err,outputx);
